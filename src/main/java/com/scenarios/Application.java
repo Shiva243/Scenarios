72a6c1829d955a39812dbce0ca997ab4e953511e
Shiva <shivav809@gmail.com>
2018-05-21 11:15:50 +0530

    Date Info changes

--- src/main/java/com/scenarios/Application.java
+++ src/main/java/com/scenarios/Application.java
@@ -3,11 +3,14 @@ package com.scenarios;
 
 import com.scenarios.collection.Streams;
 import com.scenarios.collection.Util;
+import static com.scenarios.DateInfo.*;
+
 
 public class Application{
     public static void main(String[] args){
         System.out.println("Welcome to Java8");
         System.out.println("Sum of Int's ["+new Streams().sumOfInt(Util.getListOfInt())+"]");
+        System.out.println("add hours for Date String ["+addHoursToDate("18/05/2018")+"]");
     }
 
 }
---
+++ src/main/java/com/scenarios/DateInfo.java
@@ -0,0 +1,26 @@
+package com.scenarios;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateInfo {
+
+    public static Date convertDateToString(String sDate){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy");
+        LocalDate dateTime = LocalDate.parse(sDate, formatter);
+        System.out.println("Inside convertDateToString ["+dateTime+"]");
+        Date date = Date.from(dateTime.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        return date;
+    }
+    public static Date addHoursToDate(String sDate){
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(convertDateToString(sDate));
+        cal.add(Calendar.HOUR, 23);
+        cal.add(Calendar.MINUTE,59);
+        cal.add(Calendar.SECOND,59);
+        return cal.getTime();
+    }
+}
